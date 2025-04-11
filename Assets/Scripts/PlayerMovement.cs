d9b3e13e7720b95c61007516bfccc82d9e5c261f
Harshil Gandhi <harshilgandhi148@gmail.com>
2025-04-10 21:34:39 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    public float speed = 6f;
+    public float gravity = -9.81f*2;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool isMoving;
+
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            //TODO: try inverted
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right*x + transform.forward*z;
+
+        controller.Move(movement*speed*Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity*Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        isMoving = lastPosition != gameObject.transform.position && isGrounded;
+
+        lastPosition = gameObject.transform.position;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BloonsTowerDefense3D
+ 
